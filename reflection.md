dbadfec4b9ce050d6c74307e2ad1894dea68bb86
arasidprojectz <47368906+arasidprojectz@users.noreply.github.com>
2020-01-20 14:12:31 -0600

    Created Reflection and Working on Tilemap

---
+++ reflection.md
@@ -0,0 +1,7 @@
+# Beta Testing
+
+# Reflection
+- hardest part of project
+- solve it?
+- do the project again
+- did i complet all the need to have list
--- sketch.js
+++ sketch.js
@@ -200,6 +200,7 @@ function gameMode() {
   
   if (states.game === "runGame") {
     background(images.fightGround);
+    generateGrid();
     createPlayer();
     playerStates();
     createNewBullets();
@@ -212,6 +213,7 @@ function gameMode() {
     bulletCollideWithViro();
     bulletCollideWithTreant();
     removeBullet();
+    bulletCollideWithTile();
     scoreCounterImages();
     updateScore();
     TimeSurvived();
@@ -385,6 +387,10 @@ function selectNewBullets() {
   }
 }
 
+function generateGrid(){ 
+  grid.makeTileMap(grid.cols, grid.rows);
+}
+
 // Get values from player class and use them in player
 function createPlayer() {
   player.movePlayer();
@@ -441,6 +447,18 @@ function removeBullet() {
   for (let i = 0; i<bullets.length; i++) {
     if (bullets[i].x < 0 || bullets[i].x > width ||
         bullets[i].y < 0 || bullets[i].y > height) {
+      bullets.splice(i, 1);
+    }
+  }
+}
+
+// Delete bullet if collide with not moveable tiles
+function bulletCollideWithTile() {
+  for (let i = 0; i<bullets.length; i++) {
+    if (bullets[i].isMoveable === true) {
+      bullets[i].update();  
+    }
+    else {
       bullets.splice(i, 1);
     }
   }
@@ -881,5 +899,34 @@ class Coins {
       // sounds.coinSound.play();
     } 
   } 
-}    
+}
+
+class Grid {
+  constructor() {
+    this.myMap = strings.tileLayout;
+    this.cols = this.myMap.length;
+    this.rows = this.myMap[0].length-1;
+    this.cellW = width/this.cols;
+    this.cellH = height/this.rows;
+  }
+
+  makeTileMap(theCols, theRows) { // Make the tiles size according to the cols and rows, and use value of strings 
+    for (let x = 0; x < theCols; x++) { 
+      for (let y = 0; y < theRows; y++) {
+        if (this.myMap[y][x] === "G") { // Ground Tile
+          image(images.groundImg, x * this.cellW, y * this.cellH, this.cellW, this.cellH);
+        }
+        else if (this.myMap[y][x] === "S") { // Stone Tile
+          image(images.stoneImg, x * this.cellW, y * this.cellH, this.cellW, this.cellH);
+        }
+        else if (this.myMap[y][x] === "W") { // Wayer Tile
+          image(images.waterImg, x * this.cellW, y * this.cellH, this.cellW, this.cellH);
+        }
+        else if (this.myMap[y][x] === ".") { // Grass Tile
+          image(images.grassImg, x * this.cellW, y * this.cellH, this.cellW, this.cellH);
+        }
+      }
+    }
+  }
+}
 
